48333ff0d0d6c2ca2897a081e14932403efd0825
gugu0das <gugu0das@gmail.com>
2016-11-26 21:53:11 +0900

    G900FXXS1CPJ7 For Galaxy S4 LTE-A

--- build_kernel-ks01.sh
+++ build_kernel-ks01.sh
@@ -5,7 +5,7 @@ export CROSS_COMPILE=$(pwd)/armeb-linux-gnueabihf-linaro_5.3.1-2016.05/bin/armeb
 
 mkdir output
 
-make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974_sec_jactive_skt_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974_sec_ks01_skt_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -j64 -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
