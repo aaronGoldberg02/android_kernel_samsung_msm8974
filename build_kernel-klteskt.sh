196dbe9ddffcf130493b7c30eac4cd39c62eb08b
gugu0das <gugu0das@gmail.com>
2016-12-29 15:07:36 +0900

    Support gugu0das Kernel for Galaxy S5

--- build_kernel-klteskt.sh
+++ build_kernel-klteskt.sh
@@ -5,7 +5,7 @@ export CROSS_COMPILE=$(pwd)/armeb-linux-gnueabihf-linaro_6.2.1-2016.11/bin/armeb
 
 mkdir output
 
-make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974_sec_jactive_skt_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_skt_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -j64 -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
