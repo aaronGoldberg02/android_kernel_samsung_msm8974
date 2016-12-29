76f2a0033c4d9150661cd18976e87bc62cc3aa2d
gugu0das <gugu0das@gmail.com>
2016-12-29 14:28:19 +0900

    Support gugu0das Kernel for N9005

---
+++ build_kernel-hlteeur.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=$(pwd)/armeb-linux-gnueabihf-linaro_6.2.1-2016.11/bin/armeb-linux-gnueabihf-
+
+mkdir output
+
+make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974_sec_hlte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -j64 -C $(pwd) O=output
+
+cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
